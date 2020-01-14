ef0d5f432f4076d54d422c57b81c0ecdf773b4a4
Jeremy Aaron Flexer <jafinoxal@gmail.com>
2020-01-14 08:22:14 -0500

    Updated draw_entities and other draw methods. (1.033).

--- Graphics/DrawChart.py
+++ Graphics/DrawChart.py
@@ -9,25 +9,27 @@ def draw_floors_and_walls(library, objects, chart):
         for y in  range(0, chart.chart_height):
             for x in range(0, chart.chart_width):
                 if chart.grids[category][(x, y)] != None:
-                    library.console_set_char_foreground(0, x+1, y+1, library.Color(objects[category][chart.grids[category][(x, y)][0]][6][0][0],
-                                                                                   objects[category][chart.grids[category][(x, y)][0]][6][0][1],
-                                                                                   objects[category][chart.grids[category][(x, y)][0]][6][0][2]))
-                    library.console_set_char_background(0, x+1, y+1, library.Color(objects[category][chart.grids[category][(x, y)][0]][6][1][0],
-                                                                                   objects[category][chart.grids[category][(x, y)][0]][6][1][1],
-                                                                                   objects[category][chart.grids[category][(x, y)][0]][6][1][2]))
-                    library.console_put_char(0, x+1, y+1, objects[category][chart.grids[category][(x, y)][0]][5], library.BKGND_SET)
+                    object_reference = objects[category][chart.grids[category][(x, y)][0]]
+                    library.console_set_char_foreground(0, x+1, y+1, library.Color(object_reference[6][0][0],
+                                                                                   object_reference[6][0][1],
+                                                                                   object_reference[6][0][2]))
+                    library.console_set_char_background(0, x+1, y+1, library.Color(object_reference[6][1][0],
+                                                                                   object_reference[6][1][1],
+                                                                                   object_reference[6][1][2]))
+                    library.console_put_char(0, x+1, y+1, object_reference[5], library.BKGND_SET)
 
 def draw_entities(library, entities, chart):
     for category in (2000, 2000):
         for y in  range(0, chart.chart_height):
             for x in range(0, chart.chart_width):
                 if chart.grids[category][(x, y)] != None:
-                    library.console_set_char_foreground(0, x, y, library.Color(entities[category][chart.grids[category][(x, y)][0]][6][0][0],
-                                                                               entities[category][chart.grids[category][(x, y)][0]][6][0][1],
-                                                                               entities[category][chart.grids[category][(x, y)][0]][6][0][2]))
-                    library.console_set_char_background(0, x, y, library.Color(entities[category][chart.grids[category][(x, y)][0]][6][1][0],
-                                                                               entities[category][chart.grids[category][(x, y)][0]][6][1][1],
-                                                                               entities[category][chart.grids[category][(x, y)][0]][6][1][2]))
-                    library.console_put_char(0, x, y, entities[category][chart.grids[category][(x, y)][0]][5], library.BKGND_SET)
+                    object_reference = entities[category][chart.grids[category][(x, y)][0]]
+                    library.console_set_char_foreground(0, x, y, library.Color(object_reference[6][0][0],
+                                                                               object_reference[6][0][1],
+                                                                               object_reference[6][0][2]))
+                    library.console_set_char_background(0, x, y, library.Color(object_reference[6][1][0],
+                                                                               object_reference[6][1][1],
+                                                                               object_reference[6][1][2]))
+                    library.console_put_char(0, x, y, object_reference[5], library.BKGND_SET)
 
 # Jafinoxal.
