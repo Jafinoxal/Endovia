cd25a19612d26237bf5db8e9cb59429a05771537
Jeremy Aaron Flexer <jafinoxal@gmail.com>
2020-01-14 02:54:50 -0500

    Still having error with draw_entity. (1.031).

--- Graphics/DrawChart.py
+++ Graphics/DrawChart.py
@@ -18,16 +18,16 @@ def draw_floors_and_walls(library, objects, chart):
                     library.console_put_char(0, x+1, y+1, objects[category][chart.grids[category][(x, y)][0]][5], library.BKGND_SET)
 
 def draw_entities(library, entities, chart):
-    for category in range(2000, 2000 + len(entities.keys())):
+    for category in (2000, 2000):
         for y in  range(0, chart.chart_height):
             for x in range(0, chart.chart_width):
                 if chart.grids[category][(x, y)] != None:
                     library.console_set_char_foreground(0, x, y, library.Color(entities[category][chart.grids[category][(x, y)][0]][6][0][0],
-                                                                                   entities[category][chart.grids[category][(x, y)][0]][6][0][1],
-                                                                                   entities[category][chart.grids[category][(x, y)][0]][6][0][2]))
+                                                                               entities[category][chart.grids[category][(x, y)][0]][6][0][1],
+                                                                               entities[category][chart.grids[category][(x, y)][0]][6][0][2]))
                     library.console_set_char_background(0, x, y, library.Color(entities[category][chart.grids[category][(x, y)][0]][6][1][0],
-                                                                                   entities[category][chart.grids[category][(x, y)][0]][6][1][1],
-                                                                                   entities[category][chart.grids[category][(x, y)][0]][6][1][2]))
+                                                                               entities[category][chart.grids[category][(x, y)][0]][6][1][1],
+                                                                               entities[category][chart.grids[category][(x, y)][0]][6][1][2]))
                     library.console_put_char(0, x, y, entities[category][chart.grids[category][(x, y)][0]][5], library.BKGND_SET)
 
 # Jafinoxal.
